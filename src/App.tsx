efc74228438cffa0a2ff7a08116ae2f3862e2c45
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2023-12-31 09:17:46 +1100

    Added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,53 @@
-import BodyWrapper from './BodyWrapper'
-import Socials from './Socials'
-import profilePicture from "./assets/profile-img.jpg"
-import { Separator } from "@/components/ui/separator"
+import BodyWrapper from "./BodyWrapper";
+import Socials from "./Socials";
+import profilePicture from "./assets/profile-img.jpg";
+import { Separator } from "@/components/ui/separator";
 
 function App() {
   return (
     <>
-    <BodyWrapper>
-      <div className="flex justify-center">
-        <div className="md:max-w-screen-lg">
-          <h1 className="text-5xl font-bold text-left my-5">
-            <span className="text-primary">Adrian </span>
-            Balbalosa
-          </h1>
-          <div className="md:flex md:flex-row-reverse md:gap-20">
-            <img src={profilePicture} alt="A photo of me" className="md:h-60 w-auto rounded-md shadow-xl mb-5 mx-auto"/>
-            <div>
-              <p className="text-left mb-5">Hey, I'm Adrian!</p>
-              <p className="text-left mb-5">
-                I study Computer Science at UNSW as a final year undergraduate student.
-                I actually swapped degrees twice, originally studying International Studies,
-                then Commerce/Arts at Macquarie Uni, before transferring to UNSW to final
-                pursue the degree I'm most passionate about!
-              </p>
-              <p className="text-left mb-5">
-                I am also currently doing my first Software Engineering Internship at MongoDB,
-                where I work on the Charts Team. Before that, I was teaching kids how to code
-                as a coding coach at Skill Samurai.
-              </p>
-              <p className="text-left mb-5">
-                My hobbies include playing electric guitar, and on rare occasions, make music.
-                I am a casual powerlifter, and have participated in two novice competitions.
-                In my downtime, I enjoy playing video games like Elden Ring, Genshin Impact and Final Fantasy XIV. I
-                occasionally watch anime and read manga as well.
-              </p>
+      <BodyWrapper>
+        <div className="flex justify-center">
+          <div className="md:max-w-screen-lg">
+            <h1 className="text-5xl font-bold text-left my-5">
+              <span className="text-primary">Adrian </span>
+              Balbalosa
+            </h1>
+            <div className="md:flex md:flex-row-reverse md:gap-20">
+              <img
+                src={profilePicture}
+                alt="A photo of me"
+                className="md:h-60 w-auto rounded-md shadow-xl mb-5 mx-auto"
+              />
+              <div>
+                <p className="text-left mb-5">Hey, I'm Adrian!</p>
+                <p className="text-left mb-5">
+                  I study Computer Science at UNSW as a final year undergraduate student.
+                  I actually swapped degrees twice, originally studying International
+                  Studies, then Commerce/Arts at Macquarie Uni, before transferring to
+                  UNSW to final pursue the degree I'm most passionate about!
+                </p>
+                <p className="text-left mb-5">
+                  I am also currently doing my first Software Engineering Internship at
+                  MongoDB, where I work on the Charts Team. Before that, I was teaching
+                  kids how to code as a coding coach at Skill Samurai.
+                </p>
+                <p className="text-left mb-5">
+                  My hobbies include playing electric guitar, and on rare occasions, make
+                  music. I am a casual powerlifter, and have participated in two novice
+                  competitions. In my downtime, I enjoy playing video games like Elden
+                  Ring, Genshin Impact and Final Fantasy XIV. I occasionally watch anime
+                  and read manga as well.
+                </p>
+              </div>
             </div>
+            <Separator />
+            <Socials />
           </div>
-          <Separator />
-          <Socials />
         </div>
-      </div>
-    </BodyWrapper>
+      </BodyWrapper>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Socials.tsx
+++ src/Socials.tsx
@@ -1,12 +1,17 @@
-import { GitHubLogoIcon, InstagramLogoIcon, DiscordLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import {
+  GitHubLogoIcon,
+  InstagramLogoIcon,
+  DiscordLogoIcon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
 
 export default function Socials() {
-    return (
-        <div className="flex gap-5 my-5 justify-center">
-            <GitHubLogoIcon className="w-10 h-10" />
-            <InstagramLogoIcon className="w-10 h-10" />
-            <LinkedInLogoIcon className="w-10 h-10" />
-            <DiscordLogoIcon className="w-10 h-10" />
-        </div>
-    )
+  return (
+    <div className="flex gap-5 my-5 justify-center">
+      <GitHubLogoIcon className="w-10 h-10" />
+      <InstagramLogoIcon className="w-10 h-10" />
+      <LinkedInLogoIcon className="w-10 h-10" />
+      <DiscordLogoIcon className="w-10 h-10" />
+    </div>
+  );
 }
