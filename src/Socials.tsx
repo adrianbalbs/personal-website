bf5c969b0294e28b22c1192967b44cf921e3bc72
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2023-12-30 21:12:01 +1100

    Changed color theme, created a root component to handle body styles

---
+++ src/Socials.tsx
@@ -0,0 +1,12 @@
+import { GitHubLogoIcon, InstagramLogoIcon, DiscordLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+
+export default function Socials() {
+    return (
+        <div className="flex gap-5 my-5 justify-center">
+            <GitHubLogoIcon className="w-10 h-10" />
+            <InstagramLogoIcon className="w-10 h-10" />
+            <LinkedInLogoIcon className="w-10 h-10" />
+            <DiscordLogoIcon className="w-10 h-10" />
+        </div>
+    )
+}
